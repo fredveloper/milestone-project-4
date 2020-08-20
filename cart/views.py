2bbde505f5bdd53a6727123ab061d8c4aebddece
Jose Mota <fredveloper@gmail.com>
2020-08-20 10:07:22 +0000

    Added adjust cart and remove from cart views

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
 
@@ -21,3 +21,26 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+def adjust_cart(request, item_id):
+    """Adjust the quantity of the specified product in the the shopping cart"""
+
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+    else:
+        cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    cart = request.session.get('cart', {})
+    cart.pop(item_id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
