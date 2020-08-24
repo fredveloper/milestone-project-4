f18f9f69228eac8c0b9ee07026b991b75f8fb8eb
Jose Mota <fredveloper@gmail.com>
2020-08-24 04:18:50 +0000

    Added checkout success logic

--- checkout/models.py
+++ checkout/models.py
@@ -34,10 +34,15 @@ class Order(models.Model):
         Update the grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-        self.delivery_cost = 15
-        self.grand_total = self.order_total + self.delivery_cost
-        self.save()
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
+        if self.order_total > 0:
+            self.delivery_cost = 15
+            self.grand_total = self.order_total + self.delivery_cost
+            self.save()
+        else:
+            self.delivery_cost = 0
+            self.grand_total = self.order_total + self.delivery_cost
+            self.save()
 
     def save(self, *args, **kwargs):
         """
